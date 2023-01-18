a931300c5ddaeb8a173487dfdb7c0944fe774ea3
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2023-01-18 12:15:27 +0000

    completed union-aliases

---
+++ union-aliases.js
@@ -0,0 +1,14 @@
+function combine(input1, input2, resultConversion) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinedAges = combine(30, 26, 'as-number');
+console.log(combinedAges);
+const combinedStringAges = combine('30', '26', 'as-text');
+console.log(combinedStringAges);
