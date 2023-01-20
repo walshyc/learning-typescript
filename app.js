e8c104f3e4aa57547c5879f4bd4d77d60949b87f
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2023-01-20 12:25:21 +0000

    finished part 1

---
+++ app.js
@@ -0,0 +1,3 @@
+let userInput;
+let userName;
+userName = userInput;
---
+++ functions.js
@@ -0,0 +1,17 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+function printResultApp(num) {
+    console.log('Result: ' + num);
+}
+function addAndHandle(n1, n2, cb) {
+    const result = n1 + n2;
+    cb(result);
+}
+printResultApp(add(5, 12));
+let combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, (result) => {
+    console.log(result);
+});
